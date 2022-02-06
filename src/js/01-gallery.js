062856f6d69cea9da61f0e144c4914fe72859412
Galina-Goncharova <galinagoncharova1112@gmail.com>
2022-02-07 00:52:10 +0300

    task 1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery")
+
+
+const listImg = galleryItems
+    .map(({ preview, original, description}) => `
+<a href="${original}"><img src="${preview}" alt="${description}"/></a>
+`)
+    .join("");
+console.log(listImg);
+
+galleryRef.innerHTML = listImg;
+
+
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function (event) {
+	 event.preventDefault();
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import '../css/common.css';
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+    const keyLocalStorage = 'videoplayer-current-time';
+
+    const onPlay = function (data) {
+    const stringData = JSON.stringify(data);
+    localStorage.setItem(keyLocalStorage, stringData);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const timePause = JSON.parse(localStorage.getItem(keyLocalStorage));
+
+player.setCurrentTime(timePause.seconds);
